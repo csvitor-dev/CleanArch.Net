42792aece709c3b7acf5199903e0787d097fcd30
csvitor-dev <csvitor.dev@gmail.com>
2024-12-08 18:23:29 -0300

    first commit
    SUMMARY: a simple application to learn and pratice about clean
    architecture

---
+++ CleanArch.Net.Mvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Net.Mvc.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options);
