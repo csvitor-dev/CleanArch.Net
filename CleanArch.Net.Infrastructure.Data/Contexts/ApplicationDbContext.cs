b54b3a2c5559b486e57baade1b26ad3e2b7b44e7
csvitor-dev <csvitor.dev@gmail.com>
2024-12-16 11:06:26 -0300

    refactor: replace data access from '.Mvc' to '.Infrastructure.Data'

--- CleanArch.Net.Infrastructure.Data/Contexts/ApplicationDbContext.cs
+++ CleanArch.Net.Infrastructure.Data/Contexts/ApplicationDbContext.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace CleanArch.Net.Mvc.Data;
+namespace CleanArch.Net.Infrastructure.Data.Contexts;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options);
