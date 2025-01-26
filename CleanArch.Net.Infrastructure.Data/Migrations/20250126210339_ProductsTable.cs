f14626c5a00df73e2dbf66db962ba5141219ce9b
csvitor-dev <csvitor.dev@gmail.com>
2025-01-26 18:15:57 -0300

    feat: add new migration to create `"products"` table

---
+++ CleanArch.Net.Infrastructure.Data/Migrations/20250126210339_ProductsTable.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CleanArch.Net.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 200, nullable: true),
+                    Price = table.Column<decimal>(type: "TEXT", precision: 10, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("product_id", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "products",
+                columns: new[] { "Id", "Description", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "spiral notebook 100 sheets", "Notebook", 1.575m },
+                    { 2, "small white eraser", "Eraser", 0.625m },
+                    { 3, "small plastic case", "School Pencil Case", 0.875m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "products");
+        }
+    }
+}
