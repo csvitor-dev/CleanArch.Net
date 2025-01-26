ea8c15e12a1a40af5f79aec9c61e8e637074e1e7
csvitor-dev <csvitor.dev@gmail.com>
2025-01-26 18:38:48 -0300

    fix: update `ProductRepository` structure

--- CleanArch.Net.Infrastructure.Data/Repositories/ProductRepository.cs
+++ CleanArch.Net.Infrastructure.Data/Repositories/ProductRepository.cs
@@ -5,8 +5,42 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CleanArch.Net.Infrastructure.Data.Repositories;
 
-public class ProductRepository(ApplicationDbContext _context) : IProductRepository
+public class ProductRepository(ApplicationDbContext context) : IProductRepository
 {
-    public async Task<IEnumerable<Product>> GetProductsAsync()
-        => await _context.Products.ToListAsync();
+    public async Task<IEnumerable<Product>> GetAllAsync()
+        => await context.Products.ToListAsync();
+
+    public async Task<Product> GetByIdAsync(int id)
+    {
+        var entity = await context.Products.FindAsync(id);
+        
+        if (entity is null)
+            throw new InvalidOperationException("Product not found");
+        return entity;
+    }
+
+    public async Task AddAsync(Product product)
+    {
+        await context.Products.AddAsync(product);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Product product)
+    {
+        var someEntity = await context.Products.AnyAsync(entity => entity.Id == product.Id);
+        
+        if (someEntity is false)
+            throw new InvalidOperationException($"Product with {product.Id} not exists");
+        context.Products.Update(product);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task RemoveAsync(int id)
+    {
+        var entity = await context.Products.FindAsync(id);
+        if (entity is null)
+            throw new InvalidOperationException("Product not found");
+        context.Products.Remove(entity);
+        await context.SaveChangesAsync();
+    }
 }
