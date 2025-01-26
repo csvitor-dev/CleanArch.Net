27c06e7a738a20003305a138320a0411de07408b
csvitor-dev <csvitor.dev@gmail.com>
2025-01-26 18:08:22 -0300

    fix: ensure model creation for the base class

--- CleanArch.Net.Infrastructure.Data/Contexts/ApplicationDbContext.cs
+++ CleanArch.Net.Infrastructure.Data/Contexts/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using CleanArch.Net.Domain.Entities;
+using CleanArch.Net.Infrastructure.Data.EntityConfigurations;
 
 namespace CleanArch.Net.Infrastructure.Data.Contexts;
 
@@ -9,6 +10,9 @@ public class ApplicationDbContext
 {
     public DbSet<Product> Products { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder builder) 
-        => builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
 }
