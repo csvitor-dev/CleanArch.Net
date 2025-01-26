10d8115765e988fe17e41e4ac7bbfe9db7d9b20e
csvitor-dev <csvitor.dev@gmail.com>
2025-01-26 17:43:27 -0300

    feat: expand the context class of 'ASP.NET Identity' with entity configurations from same assembly

--- CleanArch.Net.Infrastructure.Data/Contexts/ApplicationDbContext.cs
+++ CleanArch.Net.Infrastructure.Data/Contexts/ApplicationDbContext.cs
@@ -1,7 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using CleanArch.Net.Domain.Entities;
 
 namespace CleanArch.Net.Infrastructure.Data.Contexts;
 
 public class ApplicationDbContext
-    (DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options);
+    (DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder) 
+        => builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+}
