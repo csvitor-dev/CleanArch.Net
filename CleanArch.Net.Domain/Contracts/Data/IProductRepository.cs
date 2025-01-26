c61d3dc6ce4f8160a4210e2e0438644b21f1f633
csvitor-dev <csvitor.dev@gmail.com>
2025-01-26 18:24:39 -0300

    feat: improve data access contract for product entity and apply DRY principle

--- CleanArch.Net.Domain/Contracts/Data/IProductRepository.cs
+++ CleanArch.Net.Domain/Contracts/Data/IProductRepository.cs
@@ -4,5 +4,9 @@ namespace CleanArch.Net.Domain.Contracts.Data;
 
 public interface IProductRepository
 {
-    public Task<IEnumerable<Product>> GetProductsAsync();
+    public Task<IEnumerable<Product>> GetAllAsync();
+    public Task<Product> GetByIdAsync(int id);
+    public Task AddAsync(Product product);
+    public Task UpdateAsync(Product product);
+    public Task RemoveAsync(int id);
 }
