c25ec0a1bc4bf07e5fd1bf27bc4a8d5ecef1d6a7
csvitor-dev <csvitor.dev@gmail.com>
2025-02-13 10:04:05 -0300

    feat: add the rest of the operations for the `Product` resource

--- CleanArch.Net.Mvc/Controllers/ProductsController.cs
+++ CleanArch.Net.Mvc/Controllers/ProductsController.cs
@@ -20,14 +20,61 @@ public class ProductsController(IProductUseCase useCase) : Controller
 
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create(
-        [Bind("Id,Name,Description,Price")] ProductViewModel model
-    )
+    public async Task<IActionResult> Create(ProductViewModel model)
     {
         if (ModelState.IsValid is false)
             return View(model);
         await useCase.AddAsync(model);
 
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int id)
+    {
+        var model = await useCase.GetByIdAsync(id);
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(ProductViewModel model)
+    {
+        if (ModelState.IsValid is false)
+            return View(model);
+
+        try
+        {
+            await useCase.UpdateAsync(model);
+            return RedirectToAction(nameof(Index));
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { ex.Message });
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Details(int id)
+    {
+        var model = await useCase.GetByIdAsync(id);
+
+        return View(model);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var model = await useCase.GetByIdAsync(id);
+        
+        return View(model);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        await useCase.RemoveAsync(id);
+        
         return RedirectToAction(nameof(Index));
     }
 }
