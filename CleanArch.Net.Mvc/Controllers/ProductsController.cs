6cc8a0fcfd70c2ff52ae47c99e0cf21c8ec0a374
csvitor-dev <csvitor.dev@gmail.com>
2025-02-13 09:04:56 -0300

    feat: implement 'Create' resource in application

--- CleanArch.Net.Mvc/Controllers/ProductsController.cs
+++ CleanArch.Net.Mvc/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using CleanArch.Net.Application.UseCases.Product.Contracts;
+using CleanArch.Net.Application.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArch.Net.Mvc.Controllers;
@@ -9,7 +10,24 @@ public class ProductsController(IProductUseCase useCase) : Controller
     public async Task<IActionResult> Index()
     {
         var result = await useCase.GetAllAsync();
-        
+
         return View(result);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+        => View();
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(
+        [Bind("Id,Name,Description,Price")] ProductViewModel model
+    )
+    {
+        if (ModelState.IsValid is false)
+            return View(model);
+        await useCase.AddAsync(model);
+
+        return RedirectToAction(nameof(Index));
     }
 }
