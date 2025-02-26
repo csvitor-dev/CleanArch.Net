3f303ee0f29c8f4027b38fe973b7fb5060730d25
csvitor-dev <csvitor.dev@gmail.com>
2025-02-26 17:09:05 -0300

    refactor: rewrite response to `POST /emails` according with REST API

--- Api/Source/Core/CleanArch.Application/UseCases/User/Create/CreateUserResponse.cs
+++ Api/Source/Core/CleanArch.Application/UseCases/User/Create/CreateUserResponse.cs
@@ -3,6 +3,4 @@ namespace CleanArch.Application.UseCases.User.Create;
 public sealed record CreateUserResponse
 {
     public Guid Id { get; set; }
-    public string Email { get; set; } = string.Empty;
-    public string Name { get; set; } = string.Empty;
 }
