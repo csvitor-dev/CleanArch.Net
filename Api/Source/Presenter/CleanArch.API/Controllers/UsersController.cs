102d6ec8b5bac6a8879ba138979d141c4d4b91f3
csvitor-dev <csvitor.dev@gmail.com>
2025-02-27 14:58:15 -0300

    feat: implement the rest of the endpoints for `/Users`

--- Api/Source/Presenter/CleanArch.API/Controllers/UsersController.cs
+++ Api/Source/Presenter/CleanArch.API/Controllers/UsersController.cs
@@ -1,4 +1,7 @@
 using CleanArch.Application.UseCases.User.Create;
+using CleanArch.Application.UseCases.User.Delete;
+using CleanArch.Application.UseCases.User.GetAll;
+using CleanArch.Application.UseCases.User.Update;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,12 +12,44 @@ namespace CleanArch.API.Controllers;
 public class UsersController
     (IMediator mediator) : ControllerBase
 {
+    [HttpGet]
+    [ProducesResponseType(typeof(CreateUserResponse), StatusCodes.Status200OK)]
+    public async Task<ActionResult<IEnumerable<GetAllUserResponse>>> FindAll(CancellationToken token)
+    {
+        var response = await mediator.Send(new GetAllUserRequest(), token);
+
+        return Ok(response);
+    }
+
     [HttpPost]
     [ProducesResponseType(typeof(CreateUserResponse), StatusCodes.Status201Created)]
-    public async Task<IActionResult> Create(CreateUserRequest request, CancellationToken token)
+    public async Task<ActionResult> Create(CreateUserRequest request, CancellationToken token)
     {
         var response = await mediator.Send(request, token);
 
         return Created(string.Empty, response);
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(typeof(UpdateUserResponse), StatusCodes.Status200OK)]
+    public async Task<ActionResult> 
+        Update(Guid? id, UpdateUserRequest request, CancellationToken token)
+    {
+        if (id != request.Id)
+            return BadRequest();
+
+        var response = await mediator.Send(request, token);
+        return Ok(response);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(Guid? id, CancellationToken cancellationToken)
+    {
+        if (id is null)
+            return BadRequest();
+
+        var response = await mediator
+            .Send(new DeleteUserRequest(id.Value), cancellationToken);
+        return Ok(response);
     }
 }
